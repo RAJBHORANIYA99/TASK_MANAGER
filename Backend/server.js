bbb7228bc20275bf8da3cf4a4ca209eafb89baeb
rajbhoraniya <rajbhoraniya75@gmail.com>
2026-06-24 16:07:13 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: [
+        "https://taskmanager-psi-azure.vercel.app",
+        "http://localhost:5173"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/tasks", taskRouter);
